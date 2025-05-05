50a534f0e77d51af46576ffc016865d0f5aae7d0
TylerNW3994 <TylerNW3994@gmail.com>
2025-05-05 10:46:14 -0400

    Added recipe transition

--- Assets/_Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/_Assets/Scripts/Counters/StoveCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,17 +27,19 @@ public class StoveCounter : BaseCounter {
                     break;
                 case State.Cooking:
                     cookingTime += Time.deltaTime;
-                    cookingRecipeSO = GetRecipeSO(GetKitchenObject().GetKitchenObjectSO());
 
                     if (cookingTime > cookingRecipeSO.cookingTime) {
-                        cookingTime = 0f;
-                        GetKitchenObject().DestroySelf();
-
-                        KitchenObject.SpawnKitchenObject(cookingRecipeSO.output, this);
                         state = State.Cooked;
                     }
                     break;
                 case State.Cooked:
+                    cookingTime = 0f;
+                    GetKitchenObject().DestroySelf();
+
+                    KitchenObject.SpawnKitchenObject(cookingRecipeSO.output, this);
+                    cookingRecipeSO = GetCookingRecipeSO(cookingRecipeSO.output);
+
+                    state = cookingRecipeSO ? State.Cooking : State.Burned;
                     break;
                 case State.Burned:
                     break;
@@ -54,15 +57,18 @@ public class StoveCounter : BaseCounter {
                 if (HasRecipe(GetKitchenObject().GetKitchenObjectSO()) == false) {
                     return;
                 }
+                
+                cookingRecipeSO = GetCookingRecipeSO(GetKitchenObject().GetKitchenObjectSO());
 
                 InvokeOnProgressChanged(new OnProgressChangedEventArgs {
-                    progressNormalized = cookingTime / GetRecipeSO(GetKitchenObject().GetKitchenObjectSO()).cookingTime
+                    progressNormalized = cookingTime / cookingRecipeSO.cookingTime
                 });
             }
         } else {
             if (!player.HasKitchenObject()) {
                 state = State.Idle;
                 GetKitchenObject().SetKitchenObjectParent(player);
+                cookingRecipeSO = null;
             }
         }
     }
@@ -73,12 +79,12 @@ public class StoveCounter : BaseCounter {
                 KitchenObjectSO output = GetRecipeOutput(GetKitchenObject().GetKitchenObjectSO());
                 if (output == null) return;
 
-                cookingRecipeSO = GetRecipeSO(GetKitchenObject().GetKitchenObjectSO());
+                cookingRecipeSO = GetCookingRecipeSO(GetKitchenObject().GetKitchenObjectSO());
                 
                 cookingTime++;
                 InvokeOnCut();
                 InvokeOnProgressChanged(new OnProgressChangedEventArgs {
-                    progressNormalized = cookingTime / GetRecipeSO(GetKitchenObject().GetKitchenObjectSO()).cookingTime
+                    progressNormalized = cookingTime / GetCookingRecipeSO(GetKitchenObject().GetKitchenObjectSO()).cookingTime
                 });
                 if (cookingTime >= cookingRecipeSO.cookingTime) {
                     GetKitchenObject().DestroySelf();
@@ -98,12 +104,12 @@ public class StoveCounter : BaseCounter {
     }
 
     private KitchenObjectSO GetRecipeOutput(KitchenObjectSO inputKitchenObjectSO) {
-        cookingRecipeSO = GetRecipeSO(inputKitchenObjectSO);
+        cookingRecipeSO = GetCookingRecipeSO(inputKitchenObjectSO);
         if (cookingRecipeSO == null) return null;
         return cookingRecipeSO.output;
     }
 
-    private CookingRecipeSO GetRecipeSO(KitchenObjectSO inputKitchenObjectSO) {
+    private CookingRecipeSO GetCookingRecipeSO(KitchenObjectSO inputKitchenObjectSO) {
         foreach (CookingRecipeSO cookingRecipeSO in cookingRecipeSOArray) {
             if (cookingRecipeSO.input == inputKitchenObjectSO) {
                 return cookingRecipeSO;
